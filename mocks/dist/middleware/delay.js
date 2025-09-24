346076ca3202f1b895c1ffebfe5af2305d2622f1
Usuario OGTI 118 <haroldsmithcarrillogomez@gmail.com>
2025-09-24 12:47:26 -0500

    eliminar mock del api

--- mocks/dist/middleware/delay.js
+++
@@ -1,7 +0,0 @@
-export function delay(req, _res, next) {
-    const dflt = Number(process.env.DEFAULT_DELAY_MS || 0);
-    const ms = Number(req.query._delay ?? dflt);
-    if (!ms)
-        return next();
-    setTimeout(next, ms);
-}
